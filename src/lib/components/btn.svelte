b8699cbe0d2415eb2403b41a8ef71c880d7465b9
Stelfy <bastianini.alex@gmail.com>
2025-09-30 12:06:03 +0200

    Fixato quel cazzo di bottone non tondo

--- src/lib/components/btn.svelte
+++ src/lib/components/btn.svelte
@@ -3,22 +3,42 @@
 	let { ariaLabel = 'Attiva Azione', modalTarget, submissionForm } = $props();
 </script>
 
-<div class="position-fixed" style="bottom: 6em; right: 0.6em; z-index: 1050;">
+<div class="position-fixed" style="bottom: 6em; right: 0.8em; z-index: 1050;">
 	<div class="float-end">
 		<button
 			type={submissionForm ? 'submit' : 'button'}
-			class="btn rounded-circle bg-light text-primary p-0 d-flex align-items-center justify-content-center"
-			style="font-size: 4em; width: 1em; height: 1em;"
+			class="button btn"
 			aria-label={ariaLabel}
 			data-bs-toggle={modalTarget ? 'modal' : ''}
 			data-bs-target={modalTarget ?? ''}
 			form={submissionForm ?? ''}
 		>
 			{#if submissionForm}
-				<i class="bi bi-check-circle-fill"></i>
+				<i class="bi bi-check-circle-fill icon"></i>
 			{:else}
-				<i class="bi bi-plus-circle-fill"></i>
+				<i class="bi bi-plus-circle-fill icon"></i>
 			{/if}
 		</button>
 	</div>
 </div>
+
+<style>
+	.button {
+		position: relative;
+		border-radius: 50%;
+		font-size: 6em;
+		width: 100px;
+		height: 100px;
+		padding: 0;
+		background-color: white;
+		color: var(--bs-primary);
+	}
+	.icon {
+		position: absolute;
+		top: -23px;
+		left: 1px;
+	}
+	.icon::before {
+		transform: translateY(-4px);
+	}
+</style>
